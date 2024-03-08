181328a2889679d86b8fcfbbf56a15ab2ae5ce6a
thierry antoun <thierry.antoun@cea.fr>
2024-03-08 10:24:26 +0100

    No more MirrorType

--- core/src/Kokkos_CopyViews.hpp
+++ core/src/Kokkos_CopyViews.hpp
@@ -3463,27 +3463,6 @@ struct MirrorViewType {
       std::conditional_t<is_same, src_view_type, dest_view_type>;
 };
 
-// TODO: Delete this struct ?
-template <class Space, class T, class... P>
-struct MirrorType {
-  // The incoming view_type
-  using src_view_type = typename Kokkos::View<T, P...>;
-  // The memory space for the mirror view
-  using memory_space = typename Space::memory_space;
-  // Check whether it is the same memory space
-  enum {
-    is_same_memspace =
-        std::is_same<memory_space, typename src_view_type::memory_space>::value
-  };
-  // The array_layout
-  using array_layout = typename src_view_type::array_layout;
-  // The data type (we probably want it non-const since otherwise we can't even
-  // deep_copy to it.
-  using data_type = typename src_view_type::non_const_data_type;
-  // The destination view type if it is not the same memory space
-  using view_type = Kokkos::View<data_type, array_layout, Space>;
-};
-
 template <class... ViewCtorArgs>
 void check_view_ctor_args_create_mirror() {
   using alloc_prop_input = Impl::ViewCtorProp<ViewCtorArgs...>;
